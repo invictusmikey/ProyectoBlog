e6b450cc7fe54502fbf9a49f9fe3094052514946
123juanda <junieto.975@gmail.com>
2024-04-17 13:44:01 -0500

    agrego arreglo con las debidas noticias

---
+++ src/js/noticias.js
@@ -0,0 +1,33 @@
+import { img1 } from "../assets/img1.PNG";
+import { img2 } from "../assets/img2.PNG";
+import { img3 } from "../assets/img3.PNG";
+import { img4 } from "../assets/img4.PNG";
+
+const news = [
+
+    {
+        id:1,
+        Image:img1,
+        title:"Alex Pereira acabó Jamahal Hill y sigue siendo campeón",
+        description:"Alex Pereira solo necesitó poco más de 3 minutos para defender exitosamente por primera vez su cinturón luego de conectar a Jamahal Hill en el rostro con un potente gancho de izquierda para enviarlo a la lona."
+    },
+    {
+        id:2,
+        image:img2,
+        title:"Max Holloway reclama título BMF con nocaut espectacular",
+        description:"'Blessed' subió al peso ligero y finalizó a Justin Gaethje en el último segundo con un potente golpe al mentón"
+    },
+    {
+        id:3,
+        image:img3,
+        title:"Oficial el regreso de Conor McGregor contra Michael Chandler en UFC 303",
+        description:"El regreso de Conor McGregor a pelear en UFC es oficial para International Fight Week el 29 de junio en UFC 303, cuando enfrentará a Michael Chandler en su primer combate en casi tres años."
+    },
+    {
+        id:4,
+        image:img4,
+        title:'Ilia Topuria:"Queda claro que Max Holloway es el siguiente"',
+        description:"El campeón de las 145 libras felicitó a Max Holloway tras conseguir el BMF en UFC 300, pero no lo ve como una amenaza para su cinturón en la siguiente pelea."
+    }
+
+]
